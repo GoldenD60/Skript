a711b00c2289e1691f3d4db474ae1af55aaaf642
GoldenD60 <david@skillmanmedia.com>
2020-12-07 19:20:00 +0000

    Reading of files pre-complete

--- bin/skripteditor.bat
+++ bin/skripteditor.bat
@@ -5,7 +5,15 @@ echo Drag And Drop To Load .sk File
 Set /P _inputname=load(skriptfile) ^> 
 cls
 if exist "%_inputname%" (
-	echo Please Write The Code Here, thx :^)
+  cls
+  for /f "delims=" %%a in (%_inputname%) do ( 
+    echo %%a
+  )
+  goto :load
+) else (
+  cls
+  echo Error: File Doesn't Exist
+  goto :load
 )
 echo Reading ^[^] ^|
 ping localhost -n 2 >nul
